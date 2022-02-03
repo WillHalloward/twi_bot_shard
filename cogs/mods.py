06ce60c928b565a9868c2d73cf841716746a81c2
Will <willhalloward@gmail.com>
2022-02-03 20:35:03 +0100

    added await to proper places. Still fixing auto mute not working.

--- cogs/mods.py
+++ cogs/mods.py
@@ -117,7 +117,7 @@ class ModCogs(commands.Cog):
         if "Iwalkedameadowweary" in message.content and message.channel.id not in allowed_channel_ids:
             webhook = discord.SyncWebhook.from_url(secrets.webhook)
             try:
-                message.delete()
+                await message.delete()
             except discord.Forbidden:
                 logging.error(f"Failed to delete message from {message.author.id} due to Forbidden")
                 webhook.send(f"Failed to delete message from {message.author.id} due to Forbidden")
@@ -129,7 +129,7 @@ class ModCogs(commands.Cog):
             except Exception as e:
                 webhook.send(e)
             try:
-                message.author.add_roles(muted)
+                await message.author.add_roles(muted)
             except discord.Forbidden:
                 logging.error(f"Failed to add Muted role to {message.author.id} due to Forbidden")
                 webhook.send(f"Failed to add Muted role to {message.author.id} due to Forbidden")
