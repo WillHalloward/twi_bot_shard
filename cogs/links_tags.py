b6695c846a00f304dfa0670564b7f6f7c3a01830
WillHalloward <wertbonacka13147>
2022-04-10 00:56:12 +0200

    Debugging sorted tag list

--- cogs/links_tags.py
+++ cogs/links_tags.py
@@ -107,7 +107,8 @@ class LinkTags(commands.Cog, name="Links"):
         logging.error(type(query_r))
         if query_r:
             message = ""
-            for tags in sorted(query_r, key=numerical_sort):
+            # for tags in sorted(query_r, key=numerical_sort):
+            for tags in query_r:
                 logging.error(tags)
                 message = f"{message}\n`{tags['title']}`"
             await ctx.send(f"links: {message}")
