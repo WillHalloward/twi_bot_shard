b6972a7e1cced41964d88bb8cdd106a9dabbc1e2
willhalloward <willhalloward@gmail.com>
2022-09-11 03:53:22 +0200

    Testing automod rules

--- cogs/twi.py
+++ cogs/twi.py
@@ -294,7 +294,7 @@ class TwiCog(commands.Cog, name="The Wandering Inn"):
     async def get_rules(self, ctx):
         rules_list = await ctx.guild.fetch_automod_rules()
         for rule in rules_list:
-            logging.error(f"{rule}, {rule.id}, {rule.name}, {rule.trigger}, {rule.enabled}, {rule.action}")
+            logging.error(f"{rule}, {rule.id}, {rule.name}, {rule.trigger}, {rule.enabled}, {rule.actions}")
 
     # @commands.command(name="reddit")
     # async def reddit_verification(self, ctx, username):
